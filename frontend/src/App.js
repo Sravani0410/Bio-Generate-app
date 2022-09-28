1ad12b7dd5655254a1e39b328f31812803524d80
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-09-28 21:13:46 +0530

    navbar using react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bio Generator</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
