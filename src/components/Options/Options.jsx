ba0fc9b4f3194a3cec01038c61178e94c0dfbc9d
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-09-29 16:59:15 +0530

    structure

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import './Options.css';
 
 function Options(){
     return (
-        <h1>Options</h1>
+        <div className="Options_Cont">
+           <h1>Options</h1>
+        </div>
+        
     )
 }
 export default Options
