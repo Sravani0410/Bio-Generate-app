69af58313b09fd442637d53a367c80b162d0168b
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-10-01 11:54:15 +0530

    display the date in form

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,47 +1,161 @@
 import React from "react"
 import './Options.css';
 
-function Options(){
+const Options=(data)=>{
+    const {
+        name,
+        setName,
+        gender,
+        setGender,
+        location,
+        setLocation,
+        isLocation,
+        setIsLocation,
+        school,
+        setSchool,
+        isSchool,
+        setIsSchool,
+        major,
+        setMajor,
+        occupation,
+        setOccupation,
+        isOccupation,
+        setISOccupation,
+        religious,
+        setReligious,
+        isReligious,
+        setIsReligious,
+        reason,
+        setReason,
+        isReason,
+        setIsReason,
+    } = data;
+
+    const handleName = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    };
+
+    const handleGender=(e)=>{
+        e.preventDefault();
+        setGender(e.target.value)
+    }
+
+    const handleLocation=(e)=>{
+        e.preventDefault();
+        setLocation(e.target.value);
+    }
+
+    const handleSchool=(e)=>{
+        e.preventDefault();
+        setSchool(e.target.value);
+    }
+
+    const handleMajor=(e)=>{
+        e.preventDefault();
+        setMajor(e.target.value)
+    }
+
+    const handleOccupation=(e)=>{
+        e.preventDefault();
+        setOccupation(e.target.value)
+    }
+
+    const handleReligious=(e)=>{
+        e.preventDefault();
+        setReligious(e.target.value)
+    }
+
+    const handleReason=(e)=>{
+        e.preventDefault();
+        setReason(e.target.value)
+    }
     return (
         <div className="Options_Cont">
            <h2>Options</h2>
            <div className="form_data">
-                <label htmlFor="">Name</label>
-                <input type="text"  placeholder="Enter Name"/>
-                <label htmlFor="">Gender</label>
-                <select name="gender" id="">
-                    <option value="">male</option>
-                    <option value="">female</option>
+                <label for="person-name-textbox">Name</label>
+                <input
+                    type="text"
+                    name="person-name-textbox"
+                    onChange={handleName}
+                    value={name}
+                    />
+                <label for="gender">Gender</label>
+                <select name="gender" onChange={handleGender} value={gender}>
+                    <option>Female</option>
+                    <option>Male</option>
                 </select>
                 <button>Random Name</button>
            </div>
            <div className="form_data">
-               <label htmlFor="">Location</label>
-               <input type="text" />
+               <input type="checkbox" 
+               name="location"
+               onChange={()=>setIsLocation(!isLocation)}
+               checked={isLocation}
+               />
+               <label for="location">Location</label>
+               <input type="text"
+               onChange={handleLocation}
+               value={location}
+               />
                <button>Random Location</button>
            </div>
            <div className="form_data">
-                <label htmlFor="">School</label>
-                <input type="text" />
+                <input type="checkbox" 
+                name="school"
+                onChange={()=>setIsSchool(!isSchool)}
+                checked={isSchool}
+                />
+                <label for="scchool">School</label>
+                <input type="text" 
+                onChange={handleSchool}
+                value={school}
+                />
                 <button>Random School</button>
                 <br />
-                <label htmlFor="">Major</label>
-                <input type="text" />
+                <label for="major">Major</label>
+                <input type="text" 
+                onChange={handleMajor}
+                value={major}
+                />
                 <button>Random Major</button>
            </div>
            <div className="form_data">
-                <label htmlFor="">Occupation</label>
-                <input type="text" />
+                <input type="checkbox" 
+                onChange={()=>setISOccupation(!isOccupation)}
+                checked={isOccupation}
+                />
+                <label for="occupation">Occupation</label>
+                <input type="text" 
+                onChange={handleOccupation}
+                value={occupation}
+                />
                 <button>Random Occupation</button>
            </div>
            <div className="form_data">
-               <label htmlFor=""><input type="checkbox" /></label>
-               <textarea name="" id="" ></textarea>
+               <label for="religious"><input type="checkbox" 
+               onChange={()=>setIsReligious(!isReligious)}
+               checked={isReligious}
+               />Religious</label>
+               <textarea name="religious" 
+               onChange={handleReligious}
+               value={religious}
+               ></textarea>
                <button>Random text</button>
            </div>
            <div className="form_data">
-               <label htmlFor=""><input type="checkbox" /></label>
-               <textarea name="" id="" ></textarea>
+               <label for="desire"><input type="checkbox"
+               name="reason"
+               onChange={()=>setIsReason(!isReason)}
+               checked={isReason}
+               />Reason for meeting with missionaries</label>
+               <textarea 
+               type="text"
+               name="desire" 
+               onChange={handleReason}
+               value={reason}
+               ></textarea>
                <button>Restoration</button>
                <button>Plan of Salvation</button>
                <button>Gospel of Christ</button>
